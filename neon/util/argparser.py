2d2ed18e878e1359d4e03ed2fba7f814c7757c06
alvations <alvations@gmail.com>
2018-09-18 09:16:43 +0800

    Fix nonsense...

--- neon/util/argparser.py
+++ neon/util/argparser.py
@@ -78,7 +78,8 @@ class NeonArgparser(configargparse.ArgumentParser):
             kwargs['add_config_file_help'] = False
 
         self.defaults = kwargs.pop('default_overrides', dict())
-        super(NeonArgparser, self).__init__(*args, **kwargs)
+        super(NeonArgparser, self).__init__(**kwargs)
+        #super(NeonArgparser, self).__init__(*args, **kwargs)
 
         # ensure that default values are display via --help
         self.formatter_class = configargparse.ArgumentDefaultsHelpFormatter
